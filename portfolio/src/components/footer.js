4b83a516bdfe25dde5c1aa075fd734358374a46d
Guy Palmer <guypalmer@Guys-Air.attlocal.net>
2018-06-13 17:37:54 -0500

    intro header footer

---
+++ portfolio/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="container-fluid text-center footer mono">
+      <h3>Thanks for dropping in!</h3>
+      <h3>ツ</h3>
+      <br/>
+      <p className="mono">Site by <span data-tip="I mean, really? What did you expect?" className="gray-underline"><a href="https://github.com/GuyCPalmer" target="_blank" rel="noopener noreferrer">Guy</a></span> | &copy; 2018</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ portfolio/src/components/intro.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Ionicon from 'react-ionicons';
+import Typist from 'react-typist';
+import scrollToElement from 'scroll-to-element';
+
+export default class Intro extends Component {
+  handleArrowClick(el) {
+    scrollToElement(el);
+  }
+
+  render() {
+    return (
+      <div className="jumbotron">
+        <div className="intro container">
+          <div className="row">
+            <div className="col-md-offset-3 col-md-6 col-sm-12">
+              <h2 className="sans"><Typist startDelay={600} avgTypingDelay={70}><strong>&lt;Guy /&gt;</strong></Typist></h2>
+                <h2 className="mono"><span className="purple">Full-stack</span> <span className="pink">software engineer</span><br/>
+              </h2>
+              <hr/>
+              <blockquote>My <span className="highlight">passion</span> is creating great <span className="highlight">products</span> that people <span className="highlight">love</span> to use.</blockquote>
+              <div className="text-center more">
+                <span className="arrow" onClick={() => this.handleArrowClick('#about')} >
+                  <Ionicon icon="ion-ios-arrow-down" fontSize="25px" color="white"/>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
